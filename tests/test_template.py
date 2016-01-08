1c6da356ebc537202660d2481550c12133abe373
Zachary Sims <zac.sims@gmail.com>
2016-01-08 20:31:41 +1100

    Add support for outputs, conditions, mappings and metadata

--- tests/test_template.py
+++ tests/test_template.py
@@ -22,6 +22,33 @@ class TemplateParserTestCase(unittest.TestCase):
         # Assert
         self.assertSetEqual(expected_parameters, parameters)
 
+        # Check children
+        key_name = t.get_by_logical_id('KeyName')
+        self.assertEqual(3, len(key_name.children))
+
+        expected_values = {
+            'Description': 'Name of an existing EC2 KeyPair',
+            'Type': 'AWS::EC2::KeyPair::KeyName',
+            'ConstraintDescription': 'must be the name of an existing EC2 KeyPair.'
+        }
+        for c in key_name.children:
+            self.assertEqual(ElementType.property, c.element_type)
+            self.assertIn(c.key, expected_values)
+            self.assertEqual(c.value, expected_values[c.key])
+
+    def test_conditions_order_independent(self):
+        # Arrange
+        t = Template.parse_file(os.path.join(self.test_data_dir, 'conditions.template'))
+
+        # Act
+        is_ec2_classic = t.get_by_logical_id('Is-EC2-Classic')
+
+        # Assert
+        self.assertEqual(ElementType.condition, is_ec2_classic.element_type)
+        dependencies = is_ec2_classic.all_dependencies()
+        self.assertEqual(1, len(dependencies))
+        self.assertEqual('Is-EC2-VPC', list(dependencies)[0].logical_id)
+
     def test_resource_dependency_tree(self):
         # Arrange
         t = Template.parse_file(os.path.join(self.test_data_dir, 'single_server.template'))
